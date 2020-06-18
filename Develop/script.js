d8b5ba69cce8dc9d363e36a70a909039000eb3cc
Patrick Brown <pjbrown05@gmail.com>
2020-06-17 23:12:01 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,27 @@
+// USE CASES:
+    // 1) On load
+        // Planner is ready with current date under main header
+            // 9AM - 5PM, 9 timeblocks total
+        // Timeblocks are displayed color coded depending on three conditions:
+            // past = gray, present = red, future = green
+    // 2) User clicks main body of timeblock
+        // Selects input for corresponding timeblock
+        // User can type text in timelot
+    // 3) User clicks save button 
+        // Stores user input locally in browser
+    // 4) User refreshes page
+        // if input was saved then render user input to timeblock
+        // else, do nothing
+
+// Variables
+
+
+
+// Functions
+    //Moment JS
+     // Day, Month Date
+        // Not sure how to implement these yet
+        // <script src="moment.js"></script>
+        // <script>moment().format('MMMM Do YYYY, h:mm:ss a');</script>
+
+// Event listeners
